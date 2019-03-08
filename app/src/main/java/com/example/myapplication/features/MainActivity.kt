d4335844fa10029ddb2a70d39cb73cacb26be665
Aymen Rebouh <aymenmse@gmail.com>
2019-03-08 12:09:31 +0900

    Arrange project structure

--- app/src/main/java/com/example/myapplication/features/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/features/MainActivity.kt
@@ -1,18 +1,22 @@
-package com.example.myapplication
+package com.example.myapplication.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.transaction
+import com.example.myapplication.R
 import com.example.myapplication.features.home.HomeFragment
 
-class HomeActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.transaction {
-                replace(R.id.container, HomeFragment.newInstance())
+                replace(
+                    R.id.container,
+                    HomeFragment.newInstance()
+                )
             }
         }
     }
--- app/src/main/java/com/example/myapplication/features/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/features/home/HomeFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.transaction
 import androidx.lifecycle.Observer
 import com.example.myapplication.R
+import com.example.myapplication.features.profile.ProfileFragment
 import java.util.logging.Level
 import java.util.logging.Logger
 
@@ -19,6 +22,7 @@ class HomeFragment : Fragment() {
 
     // try https://developer.android.com/reference/kotlin/androidx/fragment/app/package-summary#extension-functions-summary
     private lateinit var viewModel: HomeViewModel
+    lateinit var showListButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +43,24 @@ class HomeFragment : Fragment() {
             Logger.getGlobal().log(Level.INFO, "Updated name = ${updatedName}")
         })
         viewModel.currentName.value = "Aymen"
+
+        view?.findViewById<Button>(R.id.showListButton)?.setOnClickListener {
+            fragmentManager?.transaction {
+                replace(
+                    R.id.container,
+                    UserListFragment.newInstance()
+                )
+            }
+        }
+
+        view?.findViewById<Button>(R.id.showProfileButton)?.setOnClickListener {
+            fragmentManager?.transaction {
+                replace(
+                    R.id.container,
+                    ProfileFragment.newInstance()
+                )
+            }
+        }
     }
 
 }
